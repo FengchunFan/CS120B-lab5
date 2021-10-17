8396a2c186f0921e91334ff9d2dec5bb7a8bea7e
FengchunFan <77016886+FengchunFan@users.noreply.github.com>
2021-10-16 18:45:26 -0700

    Update ffan005_lab5_part1.c

--- ffan005_lab5_part1.c
+++ ffan005_lab5_part1.c
@@ -22,7 +22,7 @@ unsigned char tmpA = 0x00;
 unsigned char output = 0x00;
     /* Insert your solution below */
     while (1) {
-    tmpA = PINA;
+    tmpA = ~PINA;
     output = 0x00;
     if((tmpA&0x00)==0x00){
 output = 0x40;
